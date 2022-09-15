06e7a587701c089ca367ad574a4c15bcbf3111e8
Nischay Agrawal <Nischay2001agrawal@gmail.com>
2022-09-15 15:18:08 +0530

    Extracted Date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,16 @@
 import "./ExpenseItem.css";
-
+import ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div className="expense-date">
-        <div className="expense-date__month">{month}</div>
-        <div className="expense-date__month">{year}</div>
-        <div className="expense-date__month">{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">Rs{props.amount}</div>
+      <div className="expense-item__price">Rs {props.amount}</div>
     </div>
   );
 }
