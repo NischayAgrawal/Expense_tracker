bae321df943def080c989ec1aab9a43acb96387b
Nischay Agrawal <Nischay2001agrawal@gmail.com>
2022-09-27 01:55:21 +0530

    Updation
    Handling condition if no expense is found for a given year

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -11,11 +11,15 @@ function Expenses(props) {
   const filteredExpenses=props.items.filter(expense=>{
     return expense.date.getFullYear().toString()===filteredYear
   })
-
+  let expensesContent = <p>No expenses found</p>
+  
+  if(filteredExpenses.length >0){
+    expensesContent = filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)
+  }
   return (
     <div className="expenses">
       <ExpenseFilter selected={filteredYear}onChangeFilter={filterChangeHandler}/>
-      {filteredExpenses.map((expense) => <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>)}
+      {expensesContent}
     </div>
   );
 }
